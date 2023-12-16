008d8905e1f792fd4a33e0d4060959eb40ceee3b
Konstantin <DoctorSager163@gmail.com>
2023-12-16 11:05:21 +0200

    vesrion with SQLite3

--- main.py
+++ main.py
@@ -1,68 +1,123 @@
+import sys
+import sqlite3  
 
+def print_menu():  
+    print ('\nВыбор пункта меню:')  
+    print('1. Добавить новый контакт')  
+    print('2. Просмотр контактов')  
+    print('3. Изменить контакт')  
+    print('4. Удаление контакта')
+    print('5. Поиск контакта')
+    print('0. Выход')
 
-finish = True
+def addcontact():
+    while True:  
+        name = input("Введите имя: ") 
+        if len(name) != 0:  
+            break  
+        else:  
+            print("Пожалуйста введите имя: ")     
+    while True:  
+        surname = input("Введите фамилию: ")  
+        if len(surname) != 0:  
+            break  
+        else:  
+            print("Пожалуйста введите фамилию")    
+    while True:  
+        num = input("Введите номер(только цифры): ")  
+        if not num.isdigit():  
+            print("Пожалйста введите численные значения")  
+            continue  
+        elif len(num) != 10:  
+            print("Пожалуйста введите 10 значный номер без пробелов, запятых, и прочих символов")  
+            continue  
+        else:  
+            break  
+    cursor.execute('''INSERT INTO phonebook (name, surname, phone_number) VALUES (?,?,?)''',
+                                                                         (name, surname, num))  
+    conn.commit()      
+    print("Новый контакт " + surname + ' ' + name + " был добавлен в телефонную книгу")
 
-while finish == True:
+def displaybook():
+    cursor.execute("SELECT surname, name, phone_number FROM phonebook ORDER BY surname")
+    results = cursor.fetchall()
+    print(results)
 
-    print("===Контакты===")
-    print("==============")
-    print('1. Просмотр всех контактов')
-    print('2. Поиск')
-    print('3. Добавить контакт')
-    print('4. Изменить контакт')
-    print('5. Выход')
-    print("==============")
+def key_pair_reception(str):
+    print ("\nВыберете пункт меню " + str + " (от 1 до 3)")  
+    print('1. Имя')  
+    print('2. Фамилия')  
+    print('3. Номер телефона')  
+    print('0. Вернуться в меню')
+    n = int(input('Ваш выбор: '))
+    if n == 1:  
+        field = "name"
+    elif n == 2:  
+        field = "surname"
+    elif n == 3:  
+        field = "phone_number"
+    else:
+        return None
+    keyword = input("\nПожалуйста введите значение: " + field )
+    keypair = field + "='" + keyword + "'"
+    return keypair
 
-    userInput = int(input('Выбор:'))
-    file = 'phoneBook.txt'
-   
-    if userInput == 1:      ### Просмотр данных из файла
-        print("==============")
-        with open(file, 'r') as data:
-            for line in data:
-                print(line)
-        print("==============")
-        
-    elif userInput == 2:    ### Поиск по файлу по совпаднию введенной строки , так и не смог додумать или найти в гугле красивый вывод в консаль при записи данных таким способом
-        print("==============")
-        findInput = input("Введите имя:")
-        lowInput = findInput.lower()
-        print("==============")
-        with open(file) as file:
-            lines = file.readlines()
-            
-            for line in lines:
-                if lowInput in line:
-                    print(line)
-                    
-    elif userInput == 3:    ### Добавление контакта
-        print("==============")
-        name = input('Введите имя: ')
-        lowerName = name.lower()
-        number = input('Введите номер: ')
-        email = input('Введите почту: ')
-        print("==============")
-        newContact = {lowerName : {'Номер' : number,'Почта' :email}}
-        
-        with open(file, 'a') as file:
-            for key, val in newContact.items():
-                file.write('{} {}\n'.format(key,val))
-       
-       
-    elif userInput == 4:    ### Изменение контакта / Удаление
-         finish = False
+def editcontacts():
+    s = key_pair_reception('searching')
+    u = key_pair_reception('updating')
+    if s != None:
+        sql = "UPDATE phonebook SET " + u + " WHERE " + s
+        cursor.execute(sql)
+        conn.commit()
+        print("Выполнено!\n")
 
-            
-    elif userInput == 5:
-        finish = False 
-            
-    
-        
-        
+def deletecontacts():
+    s = key_pair_reception('searching')
+    if s != None:
+        sql = 'DELETE FROM phonebook WHERE ' + s
+        cursor.execute(sql)
+        conn.commit()
+        print("Выполнено\n")
 
+def findcontacts():
+    s = key_pair_reception('searching')
+    if s != None:
+        sql = 'SELECT surname, name, phone_number FROM phonebook WHERE ' + s + ' ORDER BY surname'
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        print(results)
 
-
-
-
-
-
+# Основная программа
+print ('\nДобро пожаловать в телефонный справочник')
+conn = sqlite3.connect('dataBase.db')  
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS phonebook (
+                id integer PRIMARY KEY,
+                name text NOT NULL,
+                surname text,
+                phone_number text)''');
+m = -1  
+while m != 0:
+    print_menu()  
+    m = int(input('Ваш выбор: '))  
+    if m == 1:  
+        addcontact()
+        continue
+    elif m == 2:  
+        displaybook()
+        continue
+    elif m == 3:  
+        editcontacts()
+        continue
+    elif m == 4:  
+        deletecontacts()
+        continue
+    elif m == 5:  
+        findcontacts()
+        continue
+    elif m == 0:  
+        print('Программа завершена.\n')
+        conn.close()
+        sys.exit(0)  
+    else:  
+        print('Пожалуйста следуйте инструкциям')
